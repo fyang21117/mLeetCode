f0288cff7cd1c09ee57e0ac3b49233fdffc604d6
fyang21117 <yf21117@gmail.com>
2020-01-04 08:32:24 +0800

    20200104

---
+++ 1.两数之和.java
@@ -0,0 +1,27 @@
+import java.util.HashMap;
+import java.util.Map;
+
+/*
+ * @lc app=leetcode.cn id=1 lang=java
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        //2019-12-13
+        //使用一次hashmap
+        Map<Integer,Integer>map = new HashMap<>();
+        for(int i=0;i<nums.length;i++){
+            int temp = nums[i] - target;
+            if(map.containsKey(temp)){
+                return new int[]{map.get(temp),i};
+            }
+            map.put(nums[i],i);
+        }
+        throw new IllegalArgumentException("NO two sum solution");
+    }
+}
+// @lc code=end
+
